c0c671d1c8077d9fb9e16182e9f2f9bfc2f3a711
jon <9308625+junger8@users.noreply.github.com>
2021-02-23 16:11:37 -0600

    add eslint

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hi Welcome to Your Sycamore App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hi Welcome to Your Sycamore App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
